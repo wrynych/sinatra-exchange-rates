185f497f18f240ede8883adab2f1510ee45c9cac
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-03-13 04:41:54 +0000

    ch 4

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
-require 'sinatra'
+# /app.rb
+require "sinatra"
+require "sinatra/reloader"
+require "http"
 
-# Define a route for the home page
-get '/' do
-  erb :index
+# define a route
+get("/") do
+
+  # build the API url, including the API key in the query string
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATES_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+  # get the symbols from the JSON
+  # @symbols = parsed_data ...
+
+  # render a view template where I show the symbols
+  # erb(:homepage)
+end
 end
 
 # Define a route to handle requests for currency pairs
@@ -17,4 +37,3 @@ get '/:base_currency/:target_currency' do
   @target_currency = params[:target_currency]
   erb :conversion
 end
-
